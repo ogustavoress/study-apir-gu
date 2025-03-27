fdd2cf274817cdd86a41ef6e51c7027d3bdd86ec
ogustavoress <ogustavoress@gmail.com>
2025-03-27 20:32:27 -0300

    Mensagens de retorno

--- src/main/java/com/github/gustavocarvalho/study_apir/Controller/ControllerPing.java
+++ src/main/java/com/github/gustavocarvalho/study_apir/Controller/ControllerPing.java
@@ -1,4 +1,4 @@
-package com.github.gustavocarvalho.study_apir;
+package com.github.gustavocarvalho.study_apir.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ src/main/java/com/github/gustavocarvalho/study_apir/Controller/ControllerProduct.java
@@ -0,0 +1,37 @@
+package com.github.gustavocarvalho.study_apir.Controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("produtos")
+public class ControllerProduct {
+
+    @PostMapping
+    public ResponseEntity<String> create(){
+        return ResponseEntity.status(201).body("Produto Cadastrado");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> find(){
+        return ResponseEntity.status(200).body("Manga");
+    }
+
+    @PutMapping
+    public ResponseEntity<String> update(){
+        return ResponseEntity.status(200).body("Produto atualizado.");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> delete(){
+        return ResponseEntity.status(204).build();
+    }
+
+
+}
