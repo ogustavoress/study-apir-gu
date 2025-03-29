f06a723e6ce9ff7c54ecb5a56b3aac74c968284c
ogustavoress <ogustavoress@gmail.com>
2025-03-29 19:27:54 -0300

    adicionando a classe Service

---
+++ src/main/java/com/github/ogustavoress/study_apir/model/Product.java
@@ -0,0 +1,18 @@
+package com.github.ogustavoress.study_apir.model;
+
+public class Product {
+    Long id;
+    String nome;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
